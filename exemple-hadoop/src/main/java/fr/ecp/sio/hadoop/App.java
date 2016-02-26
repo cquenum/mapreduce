bc3271792ae543e023d7c645a2b4da7128ebc529
cquenum <codjo.quenum@gmail.com>
2016-02-26 11:09:53 +0100

    first commit

---
+++ exemple-hadoop/src/main/java/fr/ecp/sio/hadoop/App.java
@@ -0,0 +1,13 @@
+package fr.ecp.sio.hadoop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
